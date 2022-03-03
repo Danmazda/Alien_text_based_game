adf9956c2e42a5080a9499a9d732465cb1c89cd3
Danilo Procopio <dan_mazda@hotmail.com>
2022-03-03 12:05:19 -0300

    More history added. Unesuful console.logs deleted. User does not see text of Scenes it has already seen. Scene title added

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const buttons = document.getElementById("choices");
 const text = document.getElementById("text");
-
+const title = document.getElementById("title");
 //Criação de estado para controle do que está acontecendo no jogo.
 let userState = {
   id: 1,
   timePassed: 0,
   passengers: 0,
+  viewedScenes: [],
 };
 
 const narrator = [
   {
     id: 1,
-    text: `You are an hibernated space marine. 
-    The federation sent you to a mission in the outer rims to retrieve a egg of an unknown species.
-     You're returning home with your crew. The journey should take 5 light years, you've awakened in 6 months...`,
-    textChoice: `Voltar ao início`,
+    title: `Hibernation room`,
+    text: `You are a space marine from the Delta recon team. 
+    The federation sent you to a mission in the outer rims to retrieve an egg of an unknown species.
+    The journey should take 5 light years, to spare resources you and your crew were hibernated until you reach destination.
+    You've been suddenly awakened, your pod opens up and you can barely catch a breath, the hibernation room is filled with darkness with the only source of light being the blue dim light of your pod.
+    As far as you can see the other pods nearby are empty, there is a putrid smell in the air. Rotten food and sewer combined.
+    There's a thudding sound coming from the end of the room. To your right you remember a corridor that leads to the cafeteria.
+    To your left is a massive door that leads to the armory.
+    `,
+    textChoice: `Go back`,
     choices: [2, 3, 4],
     setState: {
       id: 1,
@@ -24,9 +31,11 @@ const narrator = [
   },
   {
     id: 2,
-    text: `Escolheu o id 2`,
-    textChoice: `Esse é o id 2`,
-    choices: [2, 3],
+    title: `Hibernation room - 2`,
+    text: `You venture through the darkness, with the thudding sound as your guide, the wicked smell is getting worse, but you keep going.
+    There's an unopened pod with no lights. A fellow crewmate is trapped inside of it, knocking the door to try to release himself. You press the button to open it.`,
+    textChoice: `Follow the noise and investigate.`,
+    choices: [3],
     setState: {
       id: 2,
       passengers: 1,
@@ -35,9 +44,12 @@ const narrator = [
   },
   {
     id: 3,
-    text: `Escolheu id 3`,
-    textChoice: `Esse é o id 3`,
-    choices: [2, 3, 4],
+    title: `Cafeteria`,
+    text: `You ignore the sound and go straight to the corridor, the smell of rotten food gets worse. You are presented with a partially opened door,
+    you squeeze your body through the aperture. You're in the cafeteria`,
+    textChoice: `Follow the corridor`,
+    // Fazer nova escolha depois
+    choices: [1],
     setState: {
       id: 3,
       timePassed: 1,
@@ -46,13 +58,14 @@ const narrator = [
   },
   {
     id: 4,
-    text: `Escolheu o id 4`,
-    textChoice: `Esse é o id 4`,
+    title: `Armory`,
+    text: `You try to open the door, there's no power, the hibernation has left you weak and you can't force the door open, after a while you give up.`,
+    textChoice: `Search for your weapons`,
     choices: [1],
     setState: {
       id: 4,
-      timePassed: 2,
-      passengers: 2,
+      timePassed: 1,
+      passengers: 0,
     },
   },
 ];
@@ -64,7 +77,6 @@ function startGame() {
     buttons.removeChild(buttons.firstChild);
   }
   //
-  console.log("The game has begun.");
   console.log(userState);
   // A cena atual será a cena em que o jogador está
   let currentScene = narrator.find((time, index) => {
@@ -72,7 +84,15 @@ function startGame() {
       return true;
     }
   });
-  text.innerText = currentScene.text;
+  // Se  o usuário já viu a cena, não mostra o texto novamente
+  if (userState.viewedScenes.includes(currentScene.id)) {
+    text.innerText = "";
+  } else {
+    text.innerText = currentScene.text;
+    userState.viewedScenes.push(currentScene.id);
+  }
+  //
+  title.innerText = currentScene.title;
   currentScene.choices.forEach((choice) => {
     // Cria um botão para cada escolha
     let newButton = document.createElement("button");
@@ -92,8 +112,8 @@ function startGame() {
         id: id,
         timePassed: userState.timePassed + timePassed,
         passengers: userState.passengers + passengers,
+        viewedScenes: userState.viewedScenes,
       };
-      console.log(userState);
       startGame();
     });
     buttons.appendChild(newButton);
