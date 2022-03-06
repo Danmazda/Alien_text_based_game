5b0b4fd7bab5743c6c3638d7f62c1c3e583d453e
Danmazda <dan_mazda@hotmail.com>
2022-03-06 10:26:17 -0300

    Narrator function for different texts, paths and finales.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let userState = {
   viewedScenes: [],
 };
 
+// Refatorar código para funcionar com a narrator function.
 const narrator = [
   {
     id: 1,
@@ -45,20 +46,7 @@ const narrator = [
       timePassed: 1,
     },
   },
-  {
-    id: 3,
-    title: `Cafeteria`,
-    text: `You ignore the sound and go straight to the corridor, the smell of rotten food gets worse. You are presented with a partially opened door,
-    you squeeze your body through the aperture. You're in the cafeteria, some lights are still on giving a slight hope to you. There may be some supplies in the pantry,  `,
-    textChoice: `Follow the corridor`,
-    // Fazer nova escolha depois
-    choices: [1],
-    setState: {
-      id: 3,
-      timePassed: 1,
-      passengers: 0,
-    },
-  },
+  ,
   {
     id: 4,
     title: `Armory`,
@@ -147,3 +135,79 @@ start.addEventListener("click", () => {
   start.remove();
   footer.remove();
 });
+
+// Narrator function - condições adicionais para textos e finais diferentes
+function narration(id, userState) {
+  let scene;
+  switch (id) {
+    case 1:
+      return {
+        title: `Hibernation room`,
+        text: `You are a space marine from the Delta recon team. 
+      The federation sent you on a mission: Secure the egg of an unknown species and take it to a secure location in the outer rims.
+      The journey should take 5 light years, to spare resources you and your crew were hibernated until you reach destination.
+      You've been suddenly awakened, your pod opens up and you can barely catch a breath, the hibernation room is filled with darkness with the only source of light being the blue dim light of your pod.
+      As far as you can see the other pods nearby are empty, there is a putrid smell in the air. Rotten food and sewer combined.
+      There's a thudding sound coming from the end of the room. To your right you remember a corridor that leads to the cafeteria.
+      To your left is a massive door that leads to the armory.
+      `,
+        textChoice: `Go back`,
+        choices: [2, 3, 4],
+      };
+    case 2:
+      return {
+        title: `Hibernation room - 2`,
+        text: `You venture through the darkness, with the thudding sound as your guide, the wicked smell is getting worse, but you keep going.
+        There's an unopened pod with no lights. A fellow crewmate is trapped inside of it, knocking the door to try to release himself. You help him open it.
+        He asks what the hell happened, but you also have no idea. You two agree on searching for supplies.`,
+        textChoice: `Follow the noise and investigate.`,
+        choices: [1],
+        id: 2,
+        passengers: 1,
+        timePassed: 1,
+      };
+    case 3:
+      scene = {
+        title: `Cafeteria`,
+        text: `You ignore the sound and go straight to the corridor, the smell of rotten food gets worse. You are presented with a partially opened door,
+        you squeeze your body through the aperture. You're in the cafeteria, some lights are still on revealing a mess of stacked tables and chairs barricating the door to the command room. There may be some supplies in the pantry,  `,
+        textChoice: `Follow the corridor`,
+        // Fazer nova escolha depois
+        choices: [1],
+        id: 3,
+        timePassed: 1,
+        passengers: 0,
+      };
+      if (userState.passengers > 0) {
+        scene.text = `You and your crewmate follow the corridor to the cafeteria, the smell of rotten food gets worse. You are presented with a partially opened door, you two squeeze
+        yourselves through the aperture. You're in the cafeteria, some lights are still on revealing a mess of stacked tables and chairs barricating the door to the command room. To your left there's a door that leaves to the pantry.
+        Passenger 1: "So the creature was let loose..."
+        You: "There must be a really fierce creature to take down that much marines."
+        Passenger 1: "We should get our weapons as soon as possible."`;
+      }
+      return scene;
+    case 4:
+      scene = {
+        id: 4,
+        title: `Armory`,
+        text: `You try to open the door, there's no power, the hibernation has left you weak and you can't force the door open, after a while you give up.`,
+        textChoice: `Search for your weapons`,
+        choices: [1],
+        timePassed: 1,
+        passengers: 0,
+      };
+      if (userState.passengers == 1) {
+        scene.text = `You both try to open the door, but is sealed shut, the hibernation has left you weak, and you can't force the door open, after a while you two give up.`;
+      }
+      if (userState.passengers == 2) {
+        scene.text = `With the backup power online you can now open the door. You three pickup armors and weapons.
+        Passenger 1: "Let the beast come."
+        You: "Time to avenge our comrades"
+        Passenger 2: "Don't get your hopes yet boys, the command room is our objetive for now."
+        `;
+      }
+    default:
+      console.log("Id not found");
+      break;
+  }
+}
