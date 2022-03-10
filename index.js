d56f073b690698e23a46cb2045004dc898155e01
Danilo Procopio <dan_mazda@hotmail.com>
2022-03-10 07:55:44 -0300

    More scenes added. Features weapon, health and first aid kit added but not yet complete. True ending started also pending complete. True Path tracking added.

--- index.js
+++ index.js
@@ -3,59 +3,89 @@ const text = document.getElementById("text");
 const title = document.getElementById("title");
 const start = document.getElementsByClassName("start")[0];
 const footer = document.querySelector("footer");
+
 //Criação de estado para controle do que está acontecendo no jogo.
 let userState = {
   id: 1,
-  timePassed: 0,
+  lurking: 0,
   passengers: 0,
-  viewedScenes: [],
+  path: [1],
+  health: 2,
+  medkit: false,
+  weapon: false,
 };
+
+const uniqueScenes = [2];
 
 ///// Jogo em si
 function startGame() {
   //Quando a função é chamada os botões são deletados
+  console.log(userState);
   while (buttons.firstChild) {
     buttons.removeChild(buttons.firstChild);
   }
-  //
-  console.log(userState);
   // A cena atual será a cena em que o jogador está
   let currentScene;
-  if (userState.timePassed >= 6) {
-    currentScene = narration(10, userState);
+  if (userState.lurking > 8) {
+    currentScene = narration(30, userState);
   } else {
     currentScene = narration(userState.id, userState);
   }
   // Se  o usuário já viu a cena, não mostra o texto novamente
-  if (userState.viewedScenes.includes(currentScene.id)) {
-    text.innerText = "";
-  } else {
-    text.innerText = currentScene.text;
-    userState.viewedScenes.push(currentScene.id);
-  }
-  //
+  // Arrumar para aparecer as diferentes cenas com diferentes quantidades de passageiros
+  // if (userState  path.includes(currentScene.id)) {
+  //   text.innerText = "";
+  // } else {}
+
+  text.innerText = currentScene.text;
   title.innerText = currentScene.title;
+
   currentScene.choices.forEach((choice) => {
     // Cria um botão para cada escolha
-    let newButton = document.createElement("button");
-    newButton.classList.add("choice");
-    let nextScene = narration(choice, userState);
-    // Texto do botão será o texto da escolha
-    newButton.innerText = nextScene.textChoice;
-    //função para ir à escolha selecionada
-    newButton.addEventListener("click", () => {
-      //Atualiza o estado, roda de novo
-      userState.id = nextScene.id;
-      if (nextScene.hasOwnProperty("passengers")) {
-        userState.passengers += nextScene.passengers;
-      }
-      if (nextScene.hasOwnProperty("timePassed")) {
-        userState.timePassed += nextScene.timePassed;
-      }
-      startGame();
-    });
+    let newButton = buttonCreate(choice, currentScene);
     buttons.appendChild(newButton);
   });
+
+  if (userState.path.length > 1) {
+    let prevScene = userState.path[userState.path.length - 1];
+    if (!uniqueScenes.includes(prevScene)) {
+      let backButton = buttonCreate(prevScene, currentScene, (goBack = true));
+      backButton.innerText = "Go back";
+      buttons.appendChild(backButton);
+    }
+  }
+}
+
+function buttonCreate(id, currentScene, goBack = false) {
+  let newButton = document.createElement("button");
+  newButton.classList.add("choice");
+  let nextScene = narration(id, userState);
+  // Texto do botão será o texto da escolha
+  newButton.innerText = nextScene.textChoice;
+  //função para ir à escolha selecionada
+  newButton.addEventListener("click", () => {
+    //Atualizações de estado
+    userState.id = nextScene.id;
+    if (nextScene.hasOwnProperty("passengers")) {
+      userState.passengers += nextScene.passengers;
+    }
+    if (nextScene.hasOwnProperty("lurking")) {
+      userState.lurking += nextScene.lurking;
+    }
+    if (nextScene.hasOwnProperty("medkit")) {
+      userState.medkit = true;
+    }
+    if (nextScene.hasOwnProperty("weapon")) {
+      userState.weapon = true;
+    }
+    if (goBack) {
+      userState.path.pop();
+    } else {
+      userState.path.push(currentScene.id);
+    }
+    startGame();
+  });
+  return newButton;
 }
 
 //Inicalizador
@@ -70,7 +100,7 @@ function narration(id, userState) {
   let scene;
   switch (id) {
     case 1:
-      return {
+      scene = {
         id: 1,
         title: `Hibernation room`,
         text: `You are a space marine from the Delta recon team. 
@@ -81,9 +111,16 @@ function narration(id, userState) {
         There's a thudding sound coming from the end of the room. To your right you remember a corridor that leads to the cafeteria.
         To your left is a massive door that leads to the armory.
         `,
-        textChoice: `Go back`,
+        textChoice: `Hibernation room `,
         choices: [2, 3, 4],
       };
+
+      if (userState.path.includes(2)) {
+        scene.choices.shift();
+      }
+
+      return scene;
+
     case 2:
       return {
         title: `Hibernation room - 2`,
@@ -91,26 +128,26 @@ function narration(id, userState) {
         There's an unopened pod with no lights. A fellow crewmate is trapped inside of it, knocking the door to try to release himself. You help him open it.
         He asks what the hell happened, but you also have no idea. You two agree on searching for supplies.`,
         textChoice: `Follow the noise and investigate.`,
-        choices: [1],
+        choices: [],
         id: 2,
         passengers: 1,
-        timePassed: 1,
+        lurking: 1,
       };
     case 3:
       scene = {
-        title: `Cafeteria`,
+        title: `Canteen and kitchen`,
         text: `You ignore the sound and go straight to the corridor, the smell of rotten food gets worse. You are presented with a partially opened door,
-        you squeeze your body through the aperture. You're in the cafeteria, some lights are still on revealing a mess of stacked tables and chairs barricating the door to the command room. There may be some supplies in the pantry,  `,
+        you squeeze your body through the aperture. You're in the canteen, some lights are still on revealing a mess of stacked tables and chairs barricading the door to the bridge. There may be some supplies in the pantry.`,
         textChoice: `Follow the corridor`,
         // Fazer nova escolha depois
-        choices: [1],
+        choices: [5, 6],
         id: 3,
-        timePassed: 1,
+        lurking: 1,
         passengers: 0,
       };
       if (userState.passengers > 0) {
-        scene.text = `You and your crewmate follow the corridor to the cafeteria, the smell of rotten food gets worse. You are presented with a partially opened door, you two squeeze
-        yourselves through the aperture. You're in the cafeteria, some lights are still on revealing a mess of stacked tables and chairs barricating the door to the command room. To your left there's a door that leaves to the pantry.
+        scene.text = `You and your crewmate follow the corridor to the canteen, the smell of rotten food gets worse. You are presented with a partially opened door, you two squeeze
+        yourselves through the aperture. You're in the canteen, some lights are still on revealing a mess of stacked tables and chairs barricading the door to the bridge. To your left there's a door that leaves to the pantry.
         Passenger 1: "So the creature was let loose..."
         You: "There must be a really fierce creature to take down that much marines."
         Passenger 1: "We should get our weapons as soon as possible."`;
@@ -122,8 +159,8 @@ function narration(id, userState) {
         title: `Armory`,
         text: `You try to open the door, there's no power, the hibernation has left you weak and you can't force the door open, after a while you give up.`,
         textChoice: `Search for your weapons`,
-        choices: [1],
-        timePassed: 1,
+        choices: [],
+        lurking: 1,
         passengers: 0,
       };
       if (userState.passengers == 1) {
@@ -133,13 +170,88 @@ function narration(id, userState) {
         scene.text = `With the backup power online you can now open the door. You three pickup armors and weapons.
         Passenger 1: "Let the beast come."
         You: "Time to avenge our comrades"
-        Passenger 2: "Don't get your hopes yet boys, the command room is our objetive for now."
+        Passenger 2: "Don't get your hopes yet boys, the bridge is our objetive for now."
         `;
+        scene.weapon = true;
       }
       return scene;
-    case 10:
+    case 5:
       scene = {
-        id: 10,
+        id: 5,
+        title: `Pantry`,
+        text: `The door to the pantry is wide open, there's where the stink is coming from, there may have an problem with refrigeration, all food is rotten. Almost all first aid kits are taken except one. Beneath the filth there's a trapdoor that goes down to the Maintenance.`,
+        textChoice: `Investigate Pantry`,
+        choices: [7, 8],
+        lurking: 1,
+      };
+      return scene;
+    case 6:
+      let captain = "";
+      scene = {
+        id: 6,
+        title: `Briefing area`,
+        text: `There's too much furniture blocking the passage, you can't move it all off the way, after a while you give up. `,
+        textChoice: `Access briefing room`,
+        choices: [],
+        lurking: 1,
+      };
+
+      if (userState.passengers === 1) {
+        scene.choices = [];
+        if (userState.lurking < 100) {
+          captain =
+            "Your captain is in the desk bleeding heavily fighting to stay alive.";
+          if (userState.medkit) {
+            captain += " You can save him with your first aid kit.";
+          } else {
+            captain += " there's nothing you can do to save him.";
+          }
+        }
+        scene.text =
+          "With all your efforts combined you clear the way to the briefing area." +
+          captain;
+      }
+      return scene;
+    case 7:
+      scene = {
+        id: 7,
+        title: `Pantry - 2`,
+        text: `You took the first aid kit.`,
+        textChoice: `Take first aid kit`,
+        choices: [],
+        medkit: true,
+      };
+      return scene;
+    case 8:
+      scene = {
+        id: 8,
+        title: `Maintenance`,
+        text: `Opening the trap door you feel an unbearable heat coming from below, it's impossible to go down without proper protection`,
+        textChoice: `Go down the trapdoor`,
+        choices: [],
+        lurking: 3,
+      };
+      if (userState.weapon) {
+        scene.text = `The three of you go down to the Maintenance tunnels, the armor softens the heat.
+        Captain - The core has gone to shit, it's only a matter of time before it explodes.
+        You - There's no way to stabilize it?
+        Captain - Not without the engineer.
+        Passenger 2 - Maybe he's still around here.
+        Captain - Or he's that pile of blood.`;
+      }
+      return scene;
+    case 29:
+      scene = {
+        id: 29,
+        title: `The end`,
+        text: ``,
+        textChoice: `Embrace your fate.`,
+      };
+      scene.text = ``;
+      return scene;
+    case 30:
+      scene = {
+        id: 30,
         title: `No escape`,
       };
       scene.text = noEscape();
@@ -157,8 +269,8 @@ function noEscape() {
   let deathScenes = [
     `A black thorned creature appear in front of you, with no weapon, you're just an easy prey. It opens its mouth and with one bite take your head off.
   The last passenger will arrive to it's destination.`,
-    `The gigantic creature got to you, with it's tail it grabs your leg and lifts you up, with it's alongated arms it rips your limbs, one at a time.`,
-    `You feel something hit your chest, lookin down you see a spear shaped tail has ripped through. The tail retracts, splattering your blood. You begin to fade while the creature feasts on your flesh. `,
+    `The gigantic creature got to you, with it's tail it grabs your leg and lifts you up, with it's elongated arms it rips your limbs, one at a time.`,
+    `You feel something hit your chest, looking down you see a spear shaped tail has ripped through. The tail retracts, splattering your blood. You begin to fade while the creature feasts on your flesh. `,
   ];
   return deathScenes[choice];
 }
